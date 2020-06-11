8599e1640155328e445b0a03335ca270702e1a55
purnomowhy <46143239+purnomowhy@users.noreply.github.com>
2020-06-11 12:11:15 +0700

    Spring Boot Web Starter
    Spring Boot Web Starter

---
+++ src/test/java/id/lock/secure/springboot/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package id.lock.secure.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
