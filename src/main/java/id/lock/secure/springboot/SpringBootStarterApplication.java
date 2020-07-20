8cdbdb9862f913c070f877de98fb2e001bf5ea5d
purnomowhy <46143239+purnomowhy@users.noreply.github.com>
2020-07-20 19:45:54 +0700

    Change Hello World

--- src/main/java/id/lock/secure/springboot/SpringBootStarterApplication.java
+++ src/main/java/id/lock/secure/springboot/SpringBootStarterApplication.java
@@ -2,9 +2,18 @@ package id.lock.secure.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class SpringBootStarterApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterApplication.class, args);
