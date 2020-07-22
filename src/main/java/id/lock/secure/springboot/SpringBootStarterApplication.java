7d7d0b46bd17472fa1618d0cde84a1381a3b215e
purnomowhy <46143239+purnomowhy@users.noreply.github.com>
2020-07-22 21:06:28 +0700

    Update GetMapping

--- src/main/java/id/lock/secure/springboot/SpringBootStarterApplication.java
+++ src/main/java/id/lock/secure/springboot/SpringBootStarterApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @SpringBootApplication
 @RestController
 public class SpringBootStarterApplication {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		return "Hello Docker World Version 3";
 	}
